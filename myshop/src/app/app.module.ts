1e309b0e9cd96ab019dd2d81943c3d999f96088d
kubaadamus <jakubadamus1991@gmail.com>
2020-03-23 21:35:06 +0100

    Laboratorium 3 - koniec

--- myshop/src/app/app.module.ts
+++ myshop/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { ProductsComponent } from './components/products/products.component';
+import { PanelTopComponent } from './components/panel-top/panel-top.component';
 
 const appRoutes: Routes = [
   { path: 'products', component: ProductsComponent },
@@ -18,11 +19,13 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     LandingComponent,
-    ProductsComponent
+    ProductsComponent,
+    PanelTopComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes),
   ],
   providers: [],
